eddd6a70a517c7f597ec8c2edfa4e76d1f716dfb
Francisco Guajardo <francisco.guajardo.v@usach.cl>
2020-11-02 17:59:29 -0300

    Adición de datos en modelos.

--- src/main/java/voluntariado/demo/models/Emergency.java
+++ src/main/java/voluntariado/demo/models/Emergency.java
@@ -1,4 +1,61 @@
 package voluntariado.demo.models;
 
+import java.sql.Date;
+
 public class Emergency {
+
+    private Integer id;
+    private String nombre;
+    private String descrip;
+    private Date finicio;
+    private Date ffin;
+    private Integer id_institucion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public Date getFinicio() {
+        return finicio;
+    }
+
+    public void setFinicio(Date finicio) {
+        this.finicio = finicio;
+    }
+
+    public Date getFfin() {
+        return ffin;
+    }
+
+    public void setFfin(Date ffin) {
+        this.ffin = ffin;
+    }
+
+    public Integer getId_institucion() {
+        return id_institucion;
+    }
+
+    public void setId_institucion(Integer id_institucion) {
+        this.id_institucion = id_institucion;
+    }
 }
--- src/main/java/voluntariado/demo/models/TaskAbility.java
+++ src/main/java/voluntariado/demo/models/TaskAbility.java
@@ -1,4 +1,32 @@
 package voluntariado.demo.models;
 
 public class TaskAbility {
+
+    private Integer id;
+    private Integer id_emehab;
+    private Integer id_habilidad;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId_emehab() {
+        return id_emehab;
+    }
+
+    public void setId_emehab(Integer id_emehab) {
+        this.id_emehab = id_emehab;
+    }
+
+    public Integer getId_habilidad() {
+        return id_habilidad;
+    }
+
+    public void setId_habilidad(Integer id_habilidad) {
+        this.id_habilidad = id_habilidad;
+    }
 }
