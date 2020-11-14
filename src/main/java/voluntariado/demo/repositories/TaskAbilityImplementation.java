3d9ce868d5642af181b558f743b717701b24138f
Esteban <esteban.silva.v@usach.cl>
2020-11-14 18:12:40 -0300

    Métodos POST,GET,DELETE(TaskAbility) listos
    verificar y probar por favor

---
+++ src/main/java/voluntariado/demo/repositories/TaskAbilityImplementation.java
@@ -0,0 +1,107 @@
+package voluntariado.demo.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import voluntariado.demo.models.TaskAbility;
+import java.util.List;
+
+@Repository
+public class TaskAbilityImplementation implements TaskAbilityRepository {
+
+    @Autowired
+    private Sql2o sql2o;
+
+    @Override
+    public List<TaskAbility> getAllTaskAbility(){
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM taskAbility").executeAndFetch(TaskAbility.class);
+        }
+        catch (Exception e)
+        {
+            System.out.println( e.getMessage() );
+            return null;
+        }
+    }
+
+
+    @Override
+    public TaskAbility getTaskAbilityById(Integer id) {
+        final String sql="SELECT * FROM taskAbility where id = :id";
+        try(Connection con = sql2o.open()){
+            TaskAbility taskAbility = con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(TaskAbility.class);
+            return taskAbility;
+        }
+        catch (Exception e)
+        {
+            System.out.println( e.getMessage() );
+            return null;
+        }
+    }
+
+
+    @Override
+    public void deleteTaskAbilityById(Integer id) {
+        final String sql = "DELETE FROM taskAbility WHERE id = :id";
+        try (Connection conn = sql2o.open()){
+            conn.createQuery(sql).addParameter("id",id).executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    @Override
+    public TaskAbility updateTaskAbilityById(Integer id, TaskAbility taskAbility) {
+        final String sql = "UPDATE taskAbility SET id_emehab = :id_emehab,id_habilidad = :id_habilidad WHERE id = :id ";
+        try (Connection conn = sql2o.open()){
+            conn.createQuery(sql,true)
+                    .addParameter("id_emehab",taskAbility.getId_emehab())
+                    .addParameter("id_habilidad",taskAbility.getId_habilidad())
+                    .addParameter("id",id)
+                    .executeUpdate();
+            taskAbility.setId(id);
+            return taskAbility;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public TaskAbility createTaskAbility(TaskAbility taskAbility) {
+        int idMax = 0;
+        try(Connection conn =sql2o.open()){
+            idMax = conn.createQuery("SELECT MAX(id) FROM taskAbility").executeScalar(Integer.class)+1;
+            conn.createQuery("INSERT INTO taskAbility(id,id_emehab,id_habilidad) " + "VALUES (:id,:id_emehab,:id_habilidad)")
+                    .addParameter("id",idMax)
+                    .addParameter("id_emehab",taskAbility.getId_emehab())
+                    .addParameter("id_habilidad",taskAbility.getId_habilidad())
+                    .executeUpdate();
+            taskAbility.setId(idMax);
+            return taskAbility;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
