c14fa5b72a20e1dbedfa82eadb4bc0c1d9861e5d
Esteban <esteban.silva.v@usach.cl>
2020-11-16 19:35:39 -0300

    Se crea metodo get a volunteerability por id
    falta parte del crud  en creacion

--- src/main/java/voluntariado/demo/repositories/VolunteerAbilityImplentation.java
+++ src/main/java/voluntariado/demo/repositories/VolunteerAbilityImplentation.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import voluntariado.demo.models.TaskAbility;
 import voluntariado.demo.models.VolunteerAbility;
 
 import java.util.List;
@@ -26,6 +27,22 @@ public class VolunteerAbilityImplentation implements VolunteerAbilityRepository 
             return null;
         }
     }
+
+    @Override
+    public VolunteerAbility getVolunteerAbilityById(Integer id) {
+        final String sql="SELECT * FROM volunteerAbility where id = :id";
+        try(Connection con = sql2o.open()){
+            VolunteerAbility volunteerAbility = con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(VolunteerAbility.class);
+            return volunteerAbility;
+        }
+        catch (Exception e)
+        {
+            System.out.println( e.getMessage() );
+            return null;
+        }
+    }
+
+
 
 
 
--- src/main/java/voluntariado/demo/repositories/VolunteerAbilityRepository.java
+++ src/main/java/voluntariado/demo/repositories/VolunteerAbilityRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface VolunteerAbilityRepository {
     public List<VolunteerAbility> getAllVolunteerAbility();
+    public VolunteerAbility getVolunteerAbilityById(Integer id);
 }
